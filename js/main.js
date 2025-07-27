4427d0f09356ef79c412bb4cb469779579c79dfe
zalalham-Sadek <zalalhamsadek@gmail.com>
2025-07-27 19:01:12 +0300

    feat:building the hero section withe slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+  
+  // menu toggle
+  
   const menuToggle = document.getElementById('menuToggle');
   const mainMenu = document.getElementById('mainMenu');
 
@@ -5,3 +8,52 @@
     mainMenu.classList.toggle('hidden');
     mainMenu.classList.toggle('flex');
   });
+
+
+
+   const slides = document.querySelectorAll('.hero-slide');
+  const nextBtn = document.getElementById('nextSlide');
+  const prevBtn = document.getElementById('prevSlide');
+
+  let current = 0;
+  let interval = null;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.style.opacity = (i === index) ? '1' : '0';
+    });
+  }
+
+  function nextSlide() {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  }
+
+  function prevSlide() {
+    current = (current - 1 + slides.length) % slides.length;
+    showSlide(current);
+  }
+
+  // arrow btn
+  nextBtn.addEventListener('click', () => {
+    nextSlide();
+    resetInterval();
+  });
+
+  prevBtn.addEventListener('click', () => {
+    prevSlide();
+    resetInterval();
+  });
+
+  // auto play
+  function startSlider() {
+    interval = setInterval(nextSlide, 5000);
+  }
+
+  function resetInterval() {
+    clearInterval(interval);
+    startSlider();
+  }
+
+  showSlide(current);
+  startSlider();
